e1b6164c552b14a2047baa028ea0b46c986c46da
Jega Pradeeba <pjega92@gmail.com>
2022-11-23 13:53:21 +0800

    added working index, show, view of suppliers - able to create and save - no css

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,7 +1,28 @@
 class SuppliersController < ApplicationController
   def index
+    @suppliers = Supplier.all
   end
 
   def show
+    @supplier = Supplier.find(params[:id])
+  end
+
+  def new
+    @supplier = Supplier.new
+  end
+
+  def create
+    @supplier = Supplier.new(supplier_params)
+    if @supplier.save
+      redirect_to suppliers_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def supplier_params
+    params.require(:supplier).permit(:name, :address, :email)
   end
 end
