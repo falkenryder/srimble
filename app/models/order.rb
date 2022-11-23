3e4a449f05639a2d1dc64ffc043e6f70199620d1
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-23 10:38:52 +0800

    added order details model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :supplier
   belongs_to :user
-#this is a test comment
+  has_many :order_details
+  has_many :products, through: :order_details
+
   validates :status, presence: true
   validates :status, inclusion: { in: %w[pending sent] }
 end
