2f3d9f2c5ad241d76c60af36cd6060ba5ff18fda
Jega Pradeeba <pjega92@gmail.com>
2022-11-23 08:47:49 +0800

    added controllers models and views suppliers order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :supplier
   belongs_to :user
-
+#this is a test comment
   validates :status, presence: true
   validates :status, inclusion: { in: %w[pending sent] }
 end
