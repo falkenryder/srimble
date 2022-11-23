b3464d836aaf145433b80ec1dd72c6c41b7a5794
Jega Pradeeba <pjega92@gmail.com>
2022-11-23 12:50:31 +0800

    added create to products resources and products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :suppliers, only: [:index, :show, :new, :create] do
-    resources :orders, only: [:index, :show, :new, :create]
-    resources :products, only: [:new]
+  resources :suppliers, only: [ :index, :show, :new, :create ] do
+    resources :orders, only: [ :index, :show, :new, :create ]
+    resources :products, only: [ :new, :create ]
   end
 end
