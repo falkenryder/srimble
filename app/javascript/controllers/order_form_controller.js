850f8e3c0e42f9bd6b23759df1e22314706f8c66
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-24 18:52:39 +0800

    added stimulus function to display price

--- app/javascript/controllers/order_form_controller.js
+++ app/javascript/controllers/order_form_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="order-form"
 export default class extends Controller {
-  static targets = ["add_detail", "template"]
+  static targets = ["add_detail", "template", "product", "productPrice"]
 
   add_association(event) {
     event.preventDefault()
@@ -16,5 +16,11 @@ export default class extends Controller {
     let item = event.target.closest(".nested-fields")
     item.querySelector("input[name*='_destroy']").value = 1
     item.style.display = "none"
+  }
+
+  display_product_price(event) {
+    event.preventDefault()
+    console.log("hello")
+
   }
 }
