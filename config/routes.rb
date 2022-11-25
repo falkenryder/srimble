e9052a4c11a9c4ceacc19cba4af9737ef433848c
Michael Thomas <mike.ahpong@gmail.com>
2022-11-25 14:46:24 +0800

    added routes to templates, rendered divs for accordian views for orders and templates

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :suppliers, only: [ :index, :show, :new, :create ] do
     resources :orders, only: [ :index, :show, :new, :create, :update ]
     resources :products, only: [ :new, :create ]
+    get 'templates', to: "orders#supplier_templates", as: :supplier_templates
   end
+
+  get 'templates/:id', to: "orders#single_template", as: :single_template
+  get 'templates', to: "orders#all_templates", as: :all_templates
 
   resources :orders, only: [:index, :show, :update ]
 end
