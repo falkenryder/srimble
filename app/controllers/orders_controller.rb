f4ea8319e7fd15819ccdcbad155fe29d763a3064
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-25 16:31:40 +0800

    added create order method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,9 +13,18 @@ class OrdersController < ApplicationController
 
   def new
     @order = Order.new
+    @addresses = @user.delivery_addresses
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.user = @user
+    @order.status = "pending" #TODO: enum this!
+    if @order.save!
+      redirect_to @order
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
@@ -27,9 +36,25 @@ class OrdersController < ApplicationController
   def set_supplier
     @supplier = Supplier.find(params[:supplier_id])
   end
-end
 
-private
-
-def set_supplier
+  def order_params
+    params.require(:order).permit(
+      :supplier_id,
+      :delivery_date,
+      :delivery_address_id,
+      :comments,
+      order_details_attributes: [:id, :_destroy, :product_id, :quantity]
+    )
+  end
 end
+# {"authenticity_token"=>"[FILTERED]",
+#   "order"=>
+#    {"supplier_id"=>"1",
+#     "delivery_date(1i)"=>"2022",
+#     "delivery_date(2i)"=>"11",
+#     "delivery_date(3i)"=>"25",
+#     "delivery_address"=>"1",
+#     "order_details_attributes"=>{"TEMPLATE_RECORD"=>{"_destroy"=>"0", "product"=>"34", "quantity"=>"1"}},
+#     "comments"=>"fuckkkk"},
+#   "commit"=>"Create Order",
+#   "supplier_id"=>"1"}
