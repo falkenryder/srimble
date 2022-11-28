eed072d128d7043b9b0a9d39c724728c4107b873
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-29 00:02:50 +0800

    Added edit page from template

--- app/javascript/controllers/order_detail_controller.js
+++ app/javascript/controllers/order_detail_controller.js
@@ -2,24 +2,41 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="order-detail"
 export default class extends Controller {
-  static targets = ["product", "productPrice", "quantity", "subtotal"]
+  static targets = ["product", "productPrice", "quantity", "subtotal", "selected"]
 
-  display_product_price(event) {
-    event.preventDefault()
-    let price = event.currentTarget.selectedOptions[0].dataset.price
+  connect () {
+    this.fetch_display_price(this.selectedTarget)
+    this.fetch_subtotal()
+    this.fetch_grand_total()
+  }
+
+  fetch_display_price(target) {
+    let price = target.selectedOptions[0].dataset.price
     this.productPriceTarget.textContent = price
-
+    this.fetch_subtotal()
   }
-  display_subtotal(event) {
-    event.preventDefault()
-    this.subtotalTarget.textContent = (parseInt(this.quantityTarget.value) || 0) * parseInt(this.productTarget.dataset.price)
 
+  fetch_subtotal() {
+    this.subtotalTarget.textContent = (parseInt(this.quantityTarget.value) || 0) * parseInt(this.productPriceTarget.textContent)
   }
-  add_to_total(event) {
-    event.preventDefault()
+
+  fetch_grand_total() {
     let sum = parseInt(this.subtotalTarget.textContent)
     let total = parseInt(document.querySelector("#grandtotal").textContent)
     document.querySelector("#grandtotal").textContent = total + sum
+  }
+
+  display_product_price(event) {
+    event.preventDefault()
+    this.fetch_display_price(event.currentTarget)
+  }
+  display_subtotal(event) {
+    event.preventDefault()
+    this.fetch_subtotal()
+  }
+  add_to_total(event) {
+    event.preventDefault()
+    this.fetch_grand_total()
 
   }
   remove_from_total(event) {
@@ -28,6 +45,5 @@ export default class extends Controller {
     let total = parseInt(document.querySelector("#grandtotal").textContent)
     document.querySelector("#grandtotal").textContent = total - sum
   }
-
 
 }
