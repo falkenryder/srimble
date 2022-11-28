600e7bea8fb1ada35fadbb1beb5ddedc23ce7fe0
Jega Pradeeba <pjega92@gmail.com>
2022-11-28 12:19:37 +0800

    added meta tags for SEO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # def default_url_options
+  #   { host: ENV["DOMAIN"] || "localhost:3000" }
+  # end
+
+  # heroku config:set DOMAIN=www.srimble.me
+  # You can check it's properly set with heroku config:get DOMAIN.
 end
