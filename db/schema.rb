1d8f95d67b41906d3e2f260477b953a0ea20b8ec
Michael Thomas <mike.ahpong@gmail.com>
2022-11-29 12:39:05 +0800

    created dependencies between inventory and other tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_035919) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_042041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,14 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_035919) do
   end
 
   create_table "inventories", force: :cascade do |t|
-    t.bigint "products_id", null: false
     t.integer "quantity_bal", null: false
     t.integer "par_bal", null: false
     t.bigint "user_id"
     t.datetime "reconciled_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["products_id"], name: "index_inventories_on_products_id"
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_inventories_on_product_id"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
@@ -100,7 +100,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_035919) do
   end
 
   add_foreign_key "delivery_addresses", "users"
-  add_foreign_key "inventories", "products", column: "products_id"
+  add_foreign_key "inventories", "products"
   add_foreign_key "inventories", "users", name: "reconciled_id"
   add_foreign_key "order_details", "products"
   add_foreign_key "orders", "delivery_addresses"
