4eb18fb380dfba45b4163881d9ed956b157788d1
Jega Pradeeba <pjega92@gmail.com>
2022-11-29 12:27:03 +0800

    added yarn db which runs drop/migrate/seed in package.json

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_29_031149) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_034220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,9 +67,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_29_031149) do
   end
 
   create_table "suppliers", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "address"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.text "address", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
